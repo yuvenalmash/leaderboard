75008e5165d54c858336a30fe6ff30d3d5e1f479
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-21 00:47:43 +0300

    add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const refreshBtn = document.getElementById("refreshBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+refreshBtn.addEventListener("click", () => {
+  console.log('refresh')
+});
+
+submitBtn.addEventListener("click", () => {
+  const name = document.getElementById("inputName").value;
+  const score = document.getElementById("inputScore").value;
+  console.log('submit')
+});
