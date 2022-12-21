e13cff19fa79ae7b4fe90610cbacae8e6b6ddb31
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-21 16:56:25 +0300

    add new list board function

--- src/index.js
+++ src/index.js
@@ -1,48 +1,63 @@
-import './style.css';
+import "./style.css";
+import { createGame, getScores } from "./modules/game.js";
 
-const refreshBtn = document.getElementById('refreshBtn');
-const submitBtn = document.getElementById('submitBtn');
+const refreshBtn = document.getElementById("refreshBtn");
+const submitBtn = document.getElementById("submitBtn");
 
 const clearBoard = () => {
-  const ul = document.getElementById('scoreList');
-  ul.innerHTML = '';
+  const ul = document.getElementById("scoreList");
+  ul.innerHTML = "";
 };
+
+// const listBoard = () => {
+//   clearBoard();
+//   let list = JSON.parse(localStorage.getItem('allEntries'));
+//   if (list === null) {
+//     list = [];
+//     localStorage.setItem('allEntries', JSON.stringify(list));
+//   }
+//   const ul = document.getElementById('scoreList');
+//   list.forEach((element) => {
+//     const li = document.createElement('li');
+//     li.innerHTML = `${element.name}: ${element.score}`;
+//     ul.appendChild(li);
+//   });
+// };
 
 const listBoard = () => {
   clearBoard();
-  let list = JSON.parse(localStorage.getItem('allEntries'));
-  if (list === null) {
-    list = [];
-    localStorage.setItem('allEntries', JSON.stringify(list));
-  }
-  const ul = document.getElementById('scoreList');
-  list.forEach((element) => {
-    const li = document.createElement('li');
-    li.innerHTML = `${element.name}: ${element.score}`;
-    ul.appendChild(li);
+  getScores().then((list) => {
+    const ul = document.getElementById("scoreList");
+
+    list.forEach((element) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${element.name}: ${element.score}`;
+      ul.appendChild(li);
+    });
   });
 };
 
-refreshBtn.addEventListener('click', () => {
+refreshBtn.addEventListener("click", () => {
   listBoard();
 });
 
-submitBtn.addEventListener('click', () => {
-  const name = document.getElementById('inputName');
-  const score = document.getElementById('inputScore');
-  const list = JSON.parse(localStorage.getItem('allEntries'));
+submitBtn.addEventListener("click", () => {
+  const name = document.getElementById("inputName");
+  const score = document.getElementById("inputScore");
+  const list = JSON.parse(localStorage.getItem("allEntries"));
   list.push({
     name: `${name.value}`,
     score: `${score.value}`,
   });
   list.sort((a, b) => b.score - a.score);
-  localStorage.setItem('allEntries', JSON.stringify(list));
+  localStorage.setItem("allEntries", JSON.stringify(list));
   listBoard();
 
   // clear inputs
-  name.value = '';
-  score.value = '';
+  name.value = "";
+  score.value = "";
 });
 
 // localStorage.setItem('allEntries', JSON.stringify([]))
 listBoard();
+getScores();
