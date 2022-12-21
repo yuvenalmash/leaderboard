ea77aa606e566146a302fcdbf8df62e1b0faff16
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-21 15:54:15 +0300

    fix output public path

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/dist/',
+    publicPath: '/',
   },
   module: {
     rules: [
