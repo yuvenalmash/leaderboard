f0c5704db67d7c111d056d82e547f6ffe271a4bc
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-21 12:43:48 +0300

    add clear board function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
 import './style.css';
 
-const refreshBtn = document.getElementById("refreshBtn");
-const submitBtn = document.getElementById("submitBtn");
+const refreshBtn = document.getElementById('refreshBtn');
+const submitBtn = document.getElementById('submitBtn');
+
+const clearBoard = () => {
+  const ul = document.getElementById('scoreList');
+  ul.innerHTML = '';
+};
 
 const listBoard = () => {
+  clearBoard();
   let list = JSON.parse(localStorage.getItem('allEntries'));
-  if (list === null){
-    list = []
+  if (list === null) {
+    list = [];
     localStorage.setItem('allEntries', JSON.stringify(list));
   }
-  const ul = document.getElementById("scoreList")
-  list.forEach(element => {
-    let li = document.createElement("li")
-    let name = document.createTextNode(`${element.name}`)
-    let score = document.createTextNode(`${element.score}`)
-    li.appendChild(name)
-    li.appendChild(score)
-    ul.appendChild(li)
+  const ul = document.getElementById('scoreList');
+  list.forEach((element) => {
+    console.log(element);
+    const li = document.createElement('li');
+    li.innerHTML = `${element.name}: ${element.score}`;
+    ul.appendChild(li);
   });
+};
 
-}
-
-refreshBtn.addEventListener("click", () => {
-  listBoard()
+refreshBtn.addEventListener('click', () => {
+  listBoard();
 });
 
-submitBtn.addEventListener("click", () => {
-  const name = document.getElementById("inputName").value;
-  const score = document.getElementById("inputScore").value;
-  let list = JSON.parse(localStorage.getItem('allEntries'));
-  list.push(
-    {name: `${name}`,
-    score: `${score}`
-  })
+submitBtn.addEventListener('click', () => {
+  const name = document.getElementById('inputName');
+  const score = document.getElementById('inputScore');
+  const list = JSON.parse(localStorage.getItem('allEntries'));
+  list.push({
+    name: `${name.value}`,
+    score: `${score.value}`,
+  });
+  list.sort((a, b) => b.score - a.score);
   localStorage.setItem('allEntries', JSON.stringify(list));
+  listBoard();
+
+  // clear inputs
+  name.value = '';
+  score.value = '';
 });
 
-listBoard()
+// localStorage.setItem('allEntries', JSON.stringify([]))
+listBoard();
