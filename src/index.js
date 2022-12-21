920244c4e5483b41fdd0ab5be9d5fefc6c1f1fff
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-21 11:18:32 +0300

    add list items function

--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import './style.css';
 const refreshBtn = document.getElementById("refreshBtn");
 const submitBtn = document.getElementById("submitBtn");
 
+const listBoard = () => {
+  let list = JSON.parse(localStorage.getItem('allEntries'));
+  if (list === null){
+    list = []
+    localStorage.setItem('allEntries', JSON.stringify(list));
+  }
+  const ul = document.getElementById("scoreList")
+  list.forEach(element => {
+    let li = document.createElement("li")
+    let name = document.createTextNode(`${element.name}`)
+    let score = document.createTextNode(`${element.score}`)
+    li.appendChild(name)
+    li.appendChild(score)
+    ul.appendChild(li)
+  });
+
+}
+
 refreshBtn.addEventListener("click", () => {
-  console.log('refresh')
+  listBoard()
 });
 
 submitBtn.addEventListener("click", () => {
   const name = document.getElementById("inputName").value;
   const score = document.getElementById("inputScore").value;
-  console.log('submit')
+  let list = JSON.parse(localStorage.getItem('allEntries'));
+  list.push(
+    {name: `${name}`,
+    score: `${score}`
+  })
+  localStorage.setItem('allEntries', JSON.stringify(list));
 });
+
+listBoard()
