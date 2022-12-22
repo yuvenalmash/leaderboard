726846fa8aa9b6fc3cb414ce04c3ffeb2281769a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-22 11:37:49 +0300

    format and fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-import "./style.css";
-import { getScores, postScores } from "./modules/game.js";
+import './style.css';
+import { getScores, postScores } from './modules/game.js';
 
-const refreshBtn = document.getElementById("refreshBtn");
-const submitBtn = document.getElementById("submitBtn");
+const refreshBtn = document.getElementById('refreshBtn');
+const submitBtn = document.getElementById('submitBtn');
 
 const clearBoard = () => {
-  const ul = document.getElementById("scoreList");
-  ul.innerHTML = "";
+  const ul = document.getElementById('scoreList');
+  ul.innerHTML = '';
 };
 
 const listBoard = async () => {
   clearBoard();
-  const ul = document.getElementById("scoreList");
+  const ul = document.getElementById('scoreList');
   const list = await getScores();
   list.sort((a, b) => b.score - a.score);
   list.forEach((element) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = `${element.user}: ${element.score}`;
     ul.appendChild(li);
   });
 };
 
-refreshBtn.addEventListener("click", () => {
+refreshBtn.addEventListener('click', () => {
   listBoard();
 });
 
-submitBtn.addEventListener("click", async () => {
-  const user = document.getElementById("inputUser");
-  const score = document.getElementById("inputScore");
+submitBtn.addEventListener('click', async () => {
+  const user = document.getElementById('inputUser');
+  const score = document.getElementById('inputScore');
   await postScores(user.value, score.value);
   listBoard();
   // clear inputs
-  user.value = "";
-  score.value = "";
+  user.value = '';
+  score.value = '';
 });
 
 listBoard();
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,14 +1,13 @@
-const baseURL =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 export const createGame = () => {
-  fetch(baseURL + "games/", {
-    method: "POST",
+  fetch(`${baseURL}games/`, {
+    method: 'POST',
     body: JSON.stringify({
       name: "Njoroge's game",
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   })
     .then((response) => response.json())
@@ -17,21 +16,20 @@ export const createGame = () => {
 // Game id = DfuN5T3Blx17JNlsQVHC
 
 export const getScores = async () => {
-  const response = await fetch(baseURL + "games/DfuN5T3Blx17JNlsQVHC/scores");
+  const response = await fetch(`${baseURL}games/DfuN5T3Blx17JNlsQVHC/scores`);
   const responseData = await response.json();
   return responseData.result;
 };
 
 export const postScores = async (user, score) => {
-  console.log(user, score);
-  const response = await fetch(baseURL + "games/DfuN5T3Blx17JNlsQVHC/scores", {
-    method: "POST",
+  const response = await fetch(`${baseURL}games/DfuN5T3Blx17JNlsQVHC/scores`, {
+    method: 'POST',
     body: JSON.stringify({
       user: `${user}`,
       score: `${score}`,
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
   const responseData = await response.json();
